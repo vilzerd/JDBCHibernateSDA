97241d8f2e4ab73736ac11fb47c99e8cb765ed12
vilzerd <patryk.bonk@o2.pl>
2023-01-11 12:48:05 +0100

    MoviesApp part 3 - other options implemented (add and display movies)

--- src/main/java/movies/Menu.java
+++ src/main/java/movies/Menu.java
@@ -1,11 +1,14 @@
 package movies;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
     //  method should operate on one level of abstraction
 
     private boolean running = true;
+    private List<Movie> movies = new ArrayList<>();
 
     public void startMenu() {
         do {
@@ -35,15 +38,55 @@ public class Menu {
     private void executeOption(int input) {
         switch (input) {
             case 1:
-                System.out.println("Adding movies");
+                addMovie();
                 break;
             case 2:
-                System.out.println("Displaying movies");
+                displayMovies();
                 break;
             case 3:
-                System.out.println("End");
-                running = false;
+                finish();
                 break;
         }
     }
+
+    private void addMovie() {
+        Movie movie = readMovieData();
+        save(movie);
+    }
+
+    private Movie readMovieData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the title of movie:");
+        String title = scanner.nextLine();
+        System.out.print("Enter release year of movie:");
+        int premiereYear = scanner.nextInt();
+        if (premiereYear < 1895 || premiereYear > 2025) {
+            System.out.println("Unreal release date given. Should be a range: 1895 - 2025.");
+            return readMovieData();
+        }
+//        scanner = new Scanner(System.in) can be use instead of line 57. as well
+        scanner.nextLine();
+        System.out.print("Enter the genre of movie:");
+        String genre = scanner.nextLine();
+        System.out.print("Enter the movie rating (1-10):");
+        int rate = scanner.nextInt();
+
+        return new Movie(title, premiereYear, genre, rate);
+    }
+
+    private void displayMovies() {
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
+    }
+
+    private void save(Movie movie) {
+        movies.add(movie);
+    }
+
+    private void finish() {
+        System.out.println("End");
+        running = false;
+    }
+
 }
--- src/main/java/movies/Movie.java
+++ src/main/java/movies/Movie.java
@@ -5,4 +5,21 @@ public class Movie {
     private int premiereYear;
     private String genre;
     private int rate;
+
+    public Movie(String title, int premiereYear, String genre, int rate) {
+        this.title = title;
+        this.premiereYear = premiereYear;
+        this.genre = genre;
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", premiereYear=" + premiereYear +
+                ", genre='" + genre + '\'' +
+                ", rate=" + rate +
+                '}';
+    }
 }
